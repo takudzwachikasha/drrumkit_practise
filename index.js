586a5f13f473ec49771a01902cc29a821a62513d
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-05-11 20:56:10 +0200

    Color change

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var numberOfButtons =  document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
-
+  this.style.color = "white";
   });
 }
