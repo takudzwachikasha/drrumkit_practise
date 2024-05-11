457975b45d75e03faa0e564661525a37ff440c86
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-05-11 20:52:51 +0200

    Button changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Loop through all elements with class "drum"
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+var numberOfButtons =  document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var audio = new Audio("sounds/tom-1.mp3");
 audio.play();
